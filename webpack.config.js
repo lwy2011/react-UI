c7a1412df8c561eec406aa079de9d0b21ef02355
liuwanyong2017 <2674706698@qq.com>
2019-05-14 00:21:36 +0800

    导出文件的配置，包括：路径，项目名称，被引入模块的定义类型umd.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
 module.exports = {
     entry: {
         index: './lib/index.tsx',
+    },
+    output: {
+        path: path.resolve('__dirname', 'dist/lib'),
+        library: 'reactUI',
+        libraryTarget: "umd"
     },
     module: {
         rules: [
