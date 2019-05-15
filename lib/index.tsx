e624f2654c7fb664e016964f30a327043444a165
liuwanyong2017 <2674706698@qq.com>
2019-05-16 01:40:12 +0800

    配置tsconfig文件和tslint.json,可以打包tsx文件了

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,10 @@
 console.log(6);
+console.log("hhaa");
 
 function sum(a: number, b: number): number {
-    console.log('test');
-    return a + b
+    console.log("test");
+    return a + b;
 }
+
+sum(3, 7);
 
