836f53cd03b0bb7b1eb72fd381ab7968dcaf363d
liuwanyong2017 <2674706698@qq.com>
2019-05-16 12:25:12 +0800

    安装react react-dom , 以及它们各自的声明依赖@types/react @types/react-dom,至此可以使用react了

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,9 @@
 console.log(69);
-console.log("aa");
+import React from "react";
+import ReactDom from "react-dom";
+
 const div = document.createElement("div");
+console.log("aa,", React, ReactDom);
 
 div.innerText = "刘万永";
 document.body.append(div);
