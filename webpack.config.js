6e797ff2662cd7719fdfaa0a06ee27094a88f3a4
liuwanyong2017 <2674706698@qq.com>
2019-05-16 13:44:12 +0800

    设置webpack不需要打包的外部依赖的库属性externals，去掉react ，react-dom这两个大头

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,19 @@ module.exports = {
             title: '永：react-UI',
             template: "index.html"
         })
-    ]
+    ],
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React',
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM',
+        },
+    }
 };
