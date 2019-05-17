96905ae95a92e24a9455aba39cb3bb9f79705613
liuwanyong2017 <2674706698@qq.com>
2019-05-17 18:48:28 +0800

    加入onClick事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -7,10 +7,11 @@ import "./icon.scss";
 
 interface IconProps {
     name: string;
+    onClick?: React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) =>
-    <svg className="yr-icon">
+    <svg className="yr-icon" onClick={props.onClick}>
         <use xlinkHref={`#${props.name}`}/>
     </svg>;
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDom from "react-dom";
 import Icon from "./icon";
 
+const iconClick: React.MouseEventHandler = (e) => {
+    console.log(e.target);
+};
 ReactDom.render(<div>
-    <Icon name="alipay"/>
+    <Icon name="alipay" onClick={iconClick}/>
     <Icon name="QQ"/>
     <Icon name="weixin"/>
 </div>, document.getElementById("app"));
