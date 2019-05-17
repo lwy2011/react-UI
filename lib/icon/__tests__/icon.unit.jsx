61ea6384aaa9c1fd48be6f42de5b8f3bdcf2c2e6
liuwanyong2017 <2674706698@qq.com>
2019-05-18 04:12:34 +0800

    点击事件的测试，用enzyme的mount方法，模拟加载后的组件，可以点击了。配置enzyme，setupTest.js。点击事件测试的，方法是用jest.fn()，expect(fn).toBeCalled()

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,8 +1,7 @@
-import renderer from "react-test-renderer";
+import renderer from "react-test-renderer";//快照
 import React from "react";
 import Icon from '../icon';
-
-import 'jest';
+import {mount} from 'enzyme'
 
 
 describe(
@@ -10,7 +9,20 @@ describe(
         it('是个svg', () => {
             const json = renderer.create(<Icon name="alipay"/>).toJSON();
             expect(json).toMatchSnapshot();
-        })
+        });
+        // it('onClick', function () {
+        //     let n = 1;
+        //     const fn = () => n = 2;
+        //     const node = mount(<Icon name="alipay" onClick={fn}/>);
+        //     node.find('svg').simulate('click');
+        //     expect(n).toEqual(1);
+        // });
+        it('onClick', function () {
+            const fn = jest.fn();
+            const node = mount(<Icon name="alipay" onClick={fn}/>);
+            node.find('svg').simulate('click');
+            expect(fn).toBeCalled()
+        });
     }
 );
 
