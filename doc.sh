b9e902ca68adb9a51ce6a1b15a609422cbfec63d
liuwanyong2017 <2674706698@qq.com>
2019-05-26 23:57:42 +0800

    update

--- doc.sh
+++ doc.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 yarn doc
-git checkout page
+git checkout gh-pages
 mv -f doc/* ./       #强制替换
 git add .
 git commit -m 'update'
 git push
 git checkout -
-#必须要保证当前代码是提交好的
+#必须要保证当前代码是提交好的  ./doc.sh运行，也就是它的相对路径就是命令
