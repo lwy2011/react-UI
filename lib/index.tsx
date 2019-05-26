c9c3e7403374e073f9104457782bd98226ae8c86
liuwanyong2017 <2674706698@qq.com>
2019-05-26 18:33:58 +0800

    新增doc，单独打包测试页面的。

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,7 @@
 import "./index.scss";
 
 export {default as Icon} from "./icon/icon";
+export {default as Button}from "./button/button";
+export {default as Buttons}from "./button/buttons";
+export {default as Dialog, alert, confirm, modal}from "./dialog/dialog";
+export {Layout, Header, Content, Footer, Aside}from "./layout/layout";
---
+++ webpack.config.example.js
@@ -0,0 +1,23 @@
+const base = require('./webpack.config.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = Object.assign({}, base, {
+    mode: 'production',
+    entry: {
+        example: './local.dev.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib/example'),
+        library: '永reactUI',
+        libraryTarget: "umd"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '永：react-UI',
+            template: "local.dev.html",
+            filename: "example.html"
+        })
+    ]
+
+});
