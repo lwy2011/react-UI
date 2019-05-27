240f1b1ec3b1f09964b2b7aa7b46d3f88a772568
liuwanyong2017 <2674706698@qq.com>
2019-05-27 19:59:22 +0800

    form的input的change，拿到数据并更新ui

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,23 +1,31 @@
-import Form from "./form";
+import Form, {newFormData} from "./form";
 import * as React from "react";
 import {useState} from "react";
 import Button from "../button/button";
 
 
 const FormExample: React.FunctionComponent = () => {
-    const [formData] = useState({
-        name: "",
+    const [formData, setFormData] = useState<newFormData>({
+        username: "",
         password: ""
     });
     const [fields] = useState([
-        {name: "username", label: "用户名", input: {type: "text"}},
+        {name: "username", label: "用户名", input: {type: "text",}},
         {name: "password", label: "密码", input: {type: "password"}},
     ]);
+    console.log(formData);
     return (
         <div className="formExample1">
-            <Form value={formData} fields={fields} buttons={[
-                <Button message="提交"/>, <Button message="返回"/>
-            ]}/>
+            <div>
+                {JSON.stringify(formData)}
+            </div>
+            <Form value={formData} fields={fields}
+                  onChange={(newData: newFormData) => {
+                      setFormData(newData);
+                  }}
+                  buttons={[
+                      <Button message="提交"/>, <Button message="返回"/>
+                  ]}/>
         </div>
     );
 };
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -3,22 +3,39 @@ import * as React from "react";
 import "./form.scss";
 import {Fragment, ReactElement} from "react";
 
+export interface newFormData {
+    [k: string]: any
+}
+
 interface Props {
-    value: { [k: string]: any },
+    value: newFormData,
     fields: Array<{ name: string, label: string, input: { type: string } }>,
-    buttons: ReactElement[]
+    buttons: ReactElement[],
+    onChange: (value: newFormData) => void
 }
 
 const Form: React.FunctionComponent<Props> = (props) => {
+    console.log(props.value);
+    const inputText = (value: string | number, name: string) => {
+        const newFormDate = {...props.value, [name]: value};
+        props.onChange(newFormDate);
+    };
     return (
         <form className="yr-form">
             <ul>
                 {
                     props.fields.map(
-                        val => <li key={val.name}>
-                            <span>{val.label}</span>
-                            <input type={val.input.type}/>
-                        </li>
+                        (data, index) => {
+                            const {name, input} = data;
+                            return (
+                                <li key={index}>
+                                    <span>{data.label}</span>
+                                    <input type={input.type}
+                                           value={props.value[name]}
+                                           onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputText(e.target.value, name)}/>
+                                </li>
+                            );
+                        }
                     )
                 }
             </ul>
