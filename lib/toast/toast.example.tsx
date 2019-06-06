7856334f30c6f10bc88a3daa270c52f7cd7c9777
liuwanyong2017 <2674706698@qq.com>
2019-06-07 01:08:47 +0800

    toast组件测试页面设计

--- lib/toast/toast.example.tsx
+++ lib/toast/toast.example.tsx
@@ -1,13 +1,67 @@
 import Button from "../button/button";
 import Toast from "./toast";
 import * as React from "react";
-
+import {MutableRefObject, useEffect, useRef, useState} from "react";
+import {scopeClassName} from "../../helpers/classes";
+import "./toast.example.scss";
 
 const ToastExample: React.FunctionComponent = () => {
-    const showToast = (message: string) => Toast(message);
+    const [time, setTime] = useState(0);
+    const [current, setCurrent] = useState();
+    const callback = () => {setTime(time - 1);};
+    const intervalRef: MutableRefObject<{ set: () => void, timer: undefined | number }> =
+        useRef({timer: 0, set: () => console.log("1")});
+    //以前傻，不知道给个默认的无关大雅的值，结果各种判断，经验之谈！！！只要保证，它只是执行方受外界变量控制，执行。
+
+    useEffect(
+        () => {intervalRef.current.set = callback;}
+    );
+    const showToast = (message: string, config?: { [k: string]: any }) => {
+        Toast(message, config);
+        const tick = () => {
+            intervalRef.current.set();
+        };
+        setTime(config && config.autoCloseDelay || 4);
+        // @ts-ignore
+        //这里对计时器的类有问题，我记得返回的是个数字，但是ts一直认为是node.Timeout，暂时忽略掉了
+        intervalRef.current.timer = setInterval(() => {tick();}, 1000);
+    };
+    useEffect(
+        () => {
+            const timer = intervalRef.current!.timer;
+            time === 0 && timer && clearInterval(timer);
+            time === 0 && timer && (intervalRef.current!.timer = undefined);
+            time === 0 && current >= 0 && setCurrent(null);
+        }, [time]
+    );
+    const sc = scopeClassName("yr-toast-example");
+    const lists = [
+        <Button message={`点我,默认4s后消失`}
+                disabled={Boolean(time)}
+                onClick={
+                    () => showToast("点我干哈？默认4s后消失！")}/>,
+        <Button message={`点我,我定2s后消失`}
+                disabled={Boolean(time)}
+                onClick={
+                    () => showToast("点我干哈？我定2s后消失", {autoCloseDelay: 2})}/>,
+    ];
     return (
         <div className="yr-toast-example">
-            <Button message={"点我"} onClick={() => showToast("点我干哈？")}/>
+            <ul>
+                {
+                    lists.map(
+                        (list, index) =>
+                            <li key={index}
+                                onClick={() => setCurrent(index)}
+                                className={sc("list")}>
+                                {list}
+                                {current === index && <span className={sc("timer")}>{time + "s"}</span>}
+                            </li>
+                    )
+                }
+            </ul>
+
+
         </div>
     );
 };
