712e2612b4c0b6d355715ba7697cb0ce1352d8bd
liuwanyong2017 <2674706698@qq.com>
2019-06-06 09:37:56 +0800

    grid改动

--- lib/grid/grid.example4.tsx
+++ lib/grid/grid.example4.tsx
@@ -6,32 +6,41 @@ import "./grid.example.scss";
 const GridExample4: React.FunctionComponent = () => {
     return (
         <div className="gridExample">
-            <Row>
-                <Col span={12}>{"col-12"}</Col>
-                <Col span={12}>{"col-12"}</Col>
-            </Row>
-            <Row>
-                <Col span={8}>{"col-8"}</Col>
-                <Col span={8}>{"col-8"}</Col>
-                <Col span={8}>{"col-8"}</Col>
-            </Row>
-            <Row>
-                <Col span={6}>{"col-6"}</Col>
-                <Col span={6}>{"col-6"}</Col>
-                <Col span={6}>{"col-6"}</Col>
-                <Col span={6}>{"col-6"}</Col>
-            </Row>
-            <Row>
-                <Col span={6}>
-                    <Row>
-                        <Col span={6}>{"col-7"}</Col>
-                        <Col span={6}>{"col-7"}</Col>
-                    </Row>
-                </Col>
-                <Col span={6}>{"col-6"}</Col>
-                <Col span={6}>{"col-6"}</Col>
-                <Col span={6}>{"col-6"}</Col>
-            </Row>
+            <div className="taobao">
+                <Row>
+                    <Col span={8}>
+                        <Row>
+                            <Col>1</Col><Col>2</Col><Col>3</Col><Col>4</Col>
+                        </Row>
+                    </Col>
+                    <Col span={12} offset={4}>
+                        <Row justify={"flex-end"}>
+                            <Col>1</Col><Col>2</Col><Col>3</Col><Col>4</Col><Col>5</Col><Col>6</Col><Col>7</Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={4}>{"col-8"}</Col>
+                    <Col span={12}>{"col-8"}</Col>
+                    <Col span={8}>{"col-8"}</Col>
+                </Row>
+                <Row className={"footer"}>
+                    <Col span={6}>
+                        <Row align={"center"} className={"imgBox"} justify={"center"}>
+                            <Col className={"imgCol"}>
+                                <img
+                                    src="https://cn.bing.com/th?id=OIP.GrGkavFEjUFdumUw1uJVuAHaHi&pid=PersonalBing&rs=1&p=0"
+                                    alt="img"/>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col span={6}>{"col-6"}</Col>
+                    <Col span={6}>{"col-6"}</Col>
+                    <Col span={6}>{"col-6"}</Col>
+                </Row>
+            </div>
+
+
             <Row gutter={24}>
                 <Col span={4} order={6}>{"col-1"}</Col>
                 <Col span={4} order={5}>{"col-2"}</Col>
