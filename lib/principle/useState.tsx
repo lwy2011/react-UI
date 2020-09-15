c2316825cd02eece05a4464cdc1d14c54d728b61
liuwanyong2017 <2674706698@qq.com>
2020-09-15 17:02:37 +0800

    增加对同步异步的测试！

--- lib/principle/useState.tsx
+++ lib/principle/useState.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Button from "../button/button";
 
 interface Obj {
@@ -6,6 +6,10 @@ interface Obj {
     age?: number
 }
 
+// const objFn = {
+//     callback: () => {
+//     }
+// };
 const UseState = () => {
     const [obj, set] = useState<Obj>({name: "liu", age: 18});
     const [obj1, set1] = useState<Obj>({name: "liu", age: 18});
@@ -32,14 +36,41 @@ const UseState = () => {
     };
     const [m, setM] = useState(1);
     const [n, setN] = useState(1);
+    const [n1, setN1] = useState(1);
+
     const addM = () => {
         setM(m + 1);
         setM(m + 2);
     };
-    const addN = () => {
-        setN(i => i + 1);
-        setN(i => i + 2);
+    const addN1 = () => {
+        setN1(i => i + 1);
+        setN1(i => i + 2);
     };
+    const addNasync = () => {
+        setN(i => i + 1);
+        console.log("第一次更新");
+        setN(i => i + 1);
+        console.log("第二次更新");
+    };
+    useEffect(
+        () => {
+            console.log("n=" + n);
+        }, [n]
+    );
+    useEffect(
+        () => {
+            setTimeout(() => {
+                //测试计时器内是否是同步的，不与class组件的setState表现一样，那个是同步的，这个依旧是异步！
+                addNasync();
+            }, 1000);
+            // objFn.callback = addNasync;
+            document.body.addEventListener("click", addNasync);
+            return () => {
+                document.body.removeEventListener("click", addNasync);
+            };
+        }, []
+    );
+
     return (
         <div className={"yr-useState"}>
             <h4>
@@ -61,7 +92,7 @@ const UseState = () => {
                 </Button>
             </div>
 
-            <Button message={"refresh"} onClick={refresh}></Button>
+            <Button message={"refresh"} onClick={refresh}/>
             <p>
                 useState的更新，适合用新的值代替旧的值，完全代替旧值。不要在旧值的基础上更新旧值！
             </p>
@@ -87,11 +118,11 @@ const UseState = () => {
             </div>
             <div style={{margin: "1em auto"}}>
                 <p>
-                    写成函数，同一块作用域内2次setSate，第一次加1第二次加2。n会加3吗？
+                    写成函数，同一块作用域内2次setSate，第一次加1第二次加2。n1会加3吗？
                     n =
-                    {n}
+                    {n1}
                 </p>
-                <Button onClick={addN} message={`n+3`}>
+                <Button onClick={addN1} message={`n1+3`}>
                 </Button>
             </div>
             <p style={{margin: "1em auto"}}>
@@ -100,7 +131,26 @@ const UseState = () => {
                 而写成函数会成功，是因为函数传的是形参指代了state，而不是一个变量。函数代表了一段逻辑：把当前的state里的m
                 加1，把当前state里的m加2。
             </p>
-
+            <h4>
+                异步执行
+            </h4>
+            <p>n={n}</p>
+            <Button onClick={addNasync} message={"async n+2"}/>
+            <p>
+                查看控制台，发现打印的都是当前的n值！异步！
+            </p>
+            <h4>
+                同步执行
+            </h4>
+            <p>
+                看控制台的打印：因为是函数式，不是class，n在函数里执行的值，成了作用域范畴的依赖了，
+                而函数式，每次更新后，就是一个新的作用域！！
+                所以，我用了监听器，监听n的变化，每次变化，打印！
+                而多次执行setState，每次执行后也打印，只是没必要打印n的值了！
+            </p>
+            <p>
+                确实是在计时器，自定义事件里，还是同步的！
+            </p>
         </div>
     );
 };
