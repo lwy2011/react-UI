6cb5d8393a0dd2d8752ed5ef9576b544ce0d8bde
liuwanyong2017 <2674706698@qq.com>
2020-09-17 10:01:32 +0800

    update

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,25 @@
-const base = require('./webpack.config.js');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const base = require("./webpack.config.js");
+const {SourceMapDevToolPlugin} = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+base.module.rules.push(
+    {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+    }
+);
 module.exports = Object.assign({}, base, {
     mode: "development",
     entry: {
-        index: './local.dev.tsx'
+        index: "./local.dev.tsx"
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '永：react-UI',
+            title: "永：react-UI",
             template: "local.dev.html"
+        }),
+        new SourceMapDevToolPlugin({
+            filename: "[file].map"
         })
     ]
 });
